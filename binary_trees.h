8a8f20e2c99a0d08cde9943f4e47954bf07a6fb6
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-02 12:04:04 +0000

    0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,6 +20,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
