01576cc6b8cc1c66121474bf90db0de0e84023a0
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-03 04:03:07 +0000

    5-binary_tree_is_root.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,12 +7,14 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
@@ -22,12 +24,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
-	new_node->right = NULL;
 
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		new_node->right->parent = new_node;
+	}
+	else
+	{
+		new_node->right = NULL;
 	}
 
 	parent->right = new_node;
