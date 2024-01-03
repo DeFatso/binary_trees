3eb4ec6470387248c892b96f63480d708a7147b1
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-03 03:12:39 +0000

    4-binary_tree_is_leaf.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
  * Return: pointer to the created node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{	 
+{
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_right - inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: is the value to store in the new node
  * Return: pointer to the created node
